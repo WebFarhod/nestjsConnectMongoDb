a8015d191bb254e62ef7b2e1346c8973363ff039
WebFarhod <https://github.com/WebFarhod>
2024-03-04 15:29:06 +0500

    create user

--- src/users/dto/CreateUser.dto.ts
+++ src/users/dto/CreateUser.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateuserDto {
   @IsNotEmpty()
@@ -6,5 +6,6 @@ export class CreateuserDto {
   username: string;
 
   @IsString()
+  @IsOptional()
   displayname?: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateuserDto } from './dto/CreateUser.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('create')
+  @UsePipes(new ValidationPipe())
+  createUser(@Body() createUserDto: CreateuserDto) {
+    console.log(createUserDto);
+    return this.usersService.createUser(createUserDto);
+  }
+}
